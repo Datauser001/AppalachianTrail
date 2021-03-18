2b9893d32032b2fe3b427742df321a8ae98a91ca
Ryan <andersonsacannon@gmail.com>
2021-03-17 22:49:31 -0500

    Added Product Details Page

--- AppalachianTrail/ClientApp/src/components/ProductDetails.js
+++ AppalachianTrail/ClientApp/src/components/ProductDetails.js
@@ -1,9 +1,26 @@
-﻿import React, { Component } from "react"
-
+﻿import React, { Component } from 'react';
 
 export default class ProductDetails extends Component {
 
-    render() {
-        return <div>Product Details</div>
+    constructor(props) {
+        super(props);
+
+        this.state = { name: 'Some interesting product', description: 'this is the product', price: 130 };
     }
+
+    render() {
+        return <div className="row">
+            <div className="col-12">
+                <div className="media">
+                    <img src="https://via.placeholder.com/600" className="mr-3" alt="Product" />
+                    <div className="media-body">
+                        <h1>{this.state.name}</h1>
+                        <p>{this.state.description}</p>
+                        <p>£{this.state.price}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    }
+
 }
